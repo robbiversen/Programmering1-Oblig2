57fd8c3bae8c976419f56884777fb6d8333cf1a1
Robert Iversen <robertiversen@live.com>
2016-02-20 15:05:56 +0100

    Added first info to Kort class

--- Programmering 1 Oblig 2/src/Kort.java
+++ Programmering 1 Oblig 2/src/Kort.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Kort {
+public abstract class Kort {
+	//private ArrayList<Navn> navn = new ArrayList<Navn>();
+	private java.util.Date dateCreated = new java.util.Date();
+	private String navn;
+	private int pin;
+	private int kortnummer;
+	private int aksesskode;
+	private boolean sperretKort;
+	private int antallkort = 100000;
+	
+	
+	public Kort(String navn, int pin) {
+		super();
+		this.dateCreated = dateCreated;
+		this.navn = navn;
+		this.pin = pin;
+		this.kortnummer = ++antallkort;
+		//this.aksesskode = aksesskode;
+		this.sperretKort = false;
+	}
+	
+	public String getNavn() {
+		return this.navn;
+	}
+	
+	public boolean isSperret() {
+		return sperretKort;
+	}
+	
+	//skal med abstract i denne metoden?
+	public boolean sjekkPIN(int pin){
+		return this.pin == pin;
+	}
 
-	//test
+	@Override
+	public String toString() {
+		return "Kort \nOpprettet: " + dateCreated + "\nNavn: " + navn + "\nPin: " + pin + "\nKortnummer: " + kortnummer
+				+ "\nAksesskode: " + aksesskode + "\nSperret: " + sperretKort;
+	}
+	
 }
+
---
+++ Programmering 1 Oblig 2/src/KortTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class KortTest {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Kort> reg = new ArrayList<Kort>();
+        
+        Kort k1 = new Ansatt("Ole Olsen", 1234);
+        Kort k2 = new Gjest("Marit Olsen", 9999);
+        reg.add(k1);
+        reg.add(k2);
+
+        for (int i=0; i<reg.size(); i++) {
+            Kort kort = (Kort)reg.get(i);
+            System.out.print(kort);
+            System.out.println("\nTest av kort: med kode 1234 er" + (kort.sjekkPIN(1234) ?  " gyldig" :  " ugyldig" ) );
+            System.out.println("\nTest av kort: med kode 9999 er" + (kort.sjekkPIN(9999) ?  " gyldig" :  " ugyldig" ) );
+        }
+    }
+}
